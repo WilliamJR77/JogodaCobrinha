37f82f9a74a8638180752d72e7f0b9120561067f
WilliamJR77 <pereiraguedes24@gmail.com>
2021-04-05 12:52:02 -0300

    definir direcoes e inpedir que a cobrinha suma

--- script.js
+++ script.js
@@ -22,7 +22,26 @@ function criarCobrinha(){
     }
 }
 
+/*Sitema de Controles no Jogo*/
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left"
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 function iniciarJogo(){
+    /*tribuir valor a nossa cobrinha para
+    ela nao ultrapassar a area definida pelo jogo
+    que nos colocamos no nosso cancas que a gente definiu
+    */
+   if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+   if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+   
     criarBG();
     criarCobrinha(); 
     
