ac7b2d31d690da01a3f74a390fdc8f9f060bb778
WilliamJR77 <pereiraguedes24@gmail.com>
2021-04-05 15:05:52 -0300

    adicionar comida para nossa cobrinha

--- script.js
+++ script.js
@@ -8,16 +8,30 @@ snake[0] = {
 }
 let direction = "right"; 
 
-function criarBG(){
+/*fazer comida aparecer em varios lugares do jogo*/
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+function criarBG()
+{
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0,  16 * box, 16 * box);   
 }
 
+/* Fazer a comida da Cobrinha para ela comer*/
+
+function drawFood(){
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
+}
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "Maroon";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
+
+        
 
     }
 }
@@ -25,14 +39,16 @@ function criarCobrinha(){
 /*Sitema de Controles no Jogo*/
 document.addEventListener('keydown', update);
 
-function update (event){
+function update (event)
+{
     if(event.keyCode == 37 && direction != "right") direction = "left"
     if(event.keyCode == 38 && direction != "down") direction = "up"
     if(event.keyCode == 39 && direction != "left") direction = "right"
     if(event.keyCode == 40 && direction != "up") direction = "down"
 }
 
-function iniciarJogo(){
+function iniciarJogo()
+{
     /*tribuir valor a nossa cobrinha para
     ela nao ultrapassar a area definida pelo jogo
     que nos colocamos no nosso cancas que a gente definiu
@@ -41,9 +57,10 @@ function iniciarJogo(){
    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-   
+
     criarBG();
-    criarCobrinha(); 
+    criarCobrinha();
+    drawFood();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -57,10 +74,11 @@ function iniciarJogo(){
 
     //criando uma nova cabeca da cobrinha com unshift
        
-   let newHead = {
+   let newHead = 
+        {
            x: snakeX,
            y: snakeY
-       }
+        }
    
        snake.unshift(newHead); // acrescenta um novo elemento a frente do outro
 }
